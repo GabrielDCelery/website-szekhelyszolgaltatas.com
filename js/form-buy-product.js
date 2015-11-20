84cb73d159e3863b39c2031d59a403cf6e5d6edd
Gabriel D. Celery <gabriel.d.celery@gmail.com>
2015-11-20 09:32:13 +0100

    Changed the object literal pattern to revealing modular pattern in form-buy-product.js

--- js/form-buy-product.js
+++ js/form-buy-product.js
@@ -2,61 +2,60 @@
 
 $(document).ready(function() {
 
-    var buyProductForm = {
-    	init: function(){
-    		this.cacheDom();
-    		this.bindEvents();
-    	},
-    	cacheDom: function(){
-    		this.$form = $("#form-order form");
-    		this.$inputProduct = this.$form.find("#product");
-    		this.$inputEmail = this.$form.find("#mail");
-    		this.$submitButton = this.$form.find("button");	
-    		this.$inputProductWarning = this.$inputProduct.next(".alert");
-    		this.$inputEmailWarning = this.$inputEmail.next(".alert");
-    	},
-    	bindEvents: function(){
-    		this.$submitButton.on("click", this.submitForm.bind(this));
-    	},
-    	checkIfProductWasSelected: function(){
-    		if(this.$inputProduct.val() != null){
-    			this.$inputProductWarning.addClass("collapse");
-    			return true;
-    		} else {
-    			this.$inputProductWarning.removeClass("collapse");
-    			return false;
-    		}
-    	},
-    	checkEmailValidity: function(){
-    		var submittedEmail = this.$inputEmail.val();
-    		var regExp = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
-    		
-    		if(regExp.test(submittedEmail)){
-    			this.$inputEmailWarning.addClass("collapse");
-    			return true;
-    		} else {
-    			this.$inputEmailWarning.removeClass("collapse");
-    			return false;
-    		}
-    	},
-    	submitForm: function(event){
-    		event.preventDefault();
-    		var checkIfProductWasSelected = this.checkIfProductWasSelected();
-    		var checkEmailValidity = this.checkEmailValidity();
-    		var formData = (this.$form).serialize();
-    		if(checkIfProductWasSelected && checkEmailValidity){
-    			$.ajax({
-    				url: "php/form-buy-product.php",
-    				method: "POST",
-    				data: formData
-    			}).done(function(data){
-                    alert("Adatok sikeresen elküldve!");
-    			})
-    		}
-    	}
+    // Cache DOM
+
+    var $form = $("#form-order form");
+    var $inputProduct = $form.find("#product");
+    var $inputEmail = $form.find("#mail");
+    var $submitButton = $form.find("button"); 
+    var $inputProductWarning = $inputProduct.next(".alert");
+    var $inputEmailWarning = $inputEmail.next(".alert");
+
+    // Bind Events
+
+    $submitButton.on("click", submitForm);
+
+    // Methods
+
+    function submitForm(){
+        event.preventDefault();
+        var checkProduct = checkIfProductWasSelected();
+        var checkEmail = checkEmailValidity();
+        var formData = ($form).serialize();
+        if(checkProduct && checkEmail){
+            console.log("success")
+            $.ajax({
+                url: "php/form-buy-product.php",
+                method: "POST",
+                data: formData
+            }).done(function(data){
+                alert("Adatok sikeresen elküldve!");
+            })
+        }  
     }
 
-    buyProductForm.init();
+    function checkIfProductWasSelected(){
+        if($inputProduct.val() != null){
+            $inputProductWarning.addClass("collapse");
+            return true;
+        } else {
+            $inputProductWarning.removeClass("collapse");
+            return false;
+        }  
+    }
+
+    function checkEmailValidity(){
+        var submittedEmail = $inputEmail.val();
+        var regExp = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
+        
+        if(regExp.test(submittedEmail)){
+            $inputEmailWarning.addClass("collapse");
+            return true;
+        } else {
+            $inputEmailWarning.removeClass("collapse");
+            return false;
+        } 
+    }
 
 }); // $(document).ready END
 
