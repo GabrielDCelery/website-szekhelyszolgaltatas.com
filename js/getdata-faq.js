b415e1d0909076ff1dde425308f6b0929da5c6ba
Gabriel D. Celery <gabriel.d.celery@gmail.com>
2015-11-20 11:38:21 +0100

    Write the code for gyik.html, getdata-faq.js and getdata-faq.php. Also changed the styling in mystyle.css for the gyik.html and changed the settings.php to be able to connect to a database

---
+++ js/getdata-faq.js
@@ -0,0 +1,46 @@
+(function(){
+
+$(document).ready(function(){
+
+	// Variables
+
+	var querystring = window.location.search.replace("?", "");
+	var searchParameters = {
+		querystring: querystring
+	}
+
+	// Caching the DOM
+
+	var $faq = $("#faq");
+	var $templateFaq = $("#template-faq").html();
+
+	// Run script
+
+	getFaqData(searchParameters, renderFaq);
+
+	// Methods
+
+	function getFaqData(searchParameters, callback){
+		var JsonData;
+		$.ajax({
+			method: "POST",
+			url: "php/getdata-faq.php",
+			data: searchParameters,
+			dataType: "json"
+		}).done(function(data){
+			JsonData = data;
+			callback(JsonData);
+		});
+	}
+
+	function renderFaq(JsonData){
+		var data = {
+			faq: JsonData
+		}
+		var rendered = Mustache.render($templateFaq, data);
+		$faq.html(rendered);
+	}
+
+})// $(document).ready END
+
+})("JQuery") // END
--- settings.php
+++ settings.php
@@ -5,6 +5,11 @@ ADMIN INFORMATION
 ***************************************************************************/
 
 $admin_email = "";
+$database_name = "practice_faq";
+$database_table_name = "questions";
+$database_hostname = "localhost";
+$database_username = "root";
+$database_password = null;
 
 /***************************************************************************
 CONTACT INFORMATION
